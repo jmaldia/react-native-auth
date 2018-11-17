c4b1f7816ea95c2280b7dca5aedd66dacfa83951
Jon Maldia <jon@maldia.net>
2018-11-17 17:34:29 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Welcome to my Auth App!</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
