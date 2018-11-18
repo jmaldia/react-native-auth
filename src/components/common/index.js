969d01b0788ad5e91f7bbec546c1df3982cb7aaa
Jon Maldia <jon@maldia.net>
2018-11-17 21:51:24 -0500

    Style input

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,4 +1,5 @@
-export * from './Button';
+export * from './Header';
 export * from './Card';
 export * from './CardSection';
-export * from './Header';
+export * from './Button';
+export * from './Input';
