b338dc9a51b6fa16671639300020ecada94f0b6a
Jon Maldia <jon@maldia.net>
2018-11-19 21:53:26 -0500

    Add code to check if log in succeeded

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { Button, Card, CardSection, Input } from './common';
 
 import firebase from 'firebase';
@@ -6,12 +7,21 @@ import firebase from 'firebase';
 class LoginForm extends Component {
     state = {
         email: '',
-        password: ''
+        password: '',
+        error: ''
     }
 
     onButtonPress() {
         const { email, password } = this.state;
-        firebase.auth().signInWithEmailAndPassword(email, password);
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .catch(() => {
+                        this.setState({
+                            error: 'Authentication Failed.'
+                        });
+                    });
+            });
     }
 
     render() {
@@ -35,6 +45,10 @@ class LoginForm extends Component {
                     />
                 </CardSection>
 
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
+
                 <CardSection>
                     <Button onPress={this.onButtonPress.bind(this)}>
                         Log in
@@ -46,3 +60,12 @@ class LoginForm extends Component {
 }
 
 export default LoginForm;
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red',
+        padding: 8
+    }
+};
